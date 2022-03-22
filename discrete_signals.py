c6a19347d92dfc40e85d95c7d5cb4bbe916a0d46
DanPuzzuoli <dan.puzzuoli@gmail.com>
2022-03-22 08:58:02 -0700

    adding new notebooks and fixing signal stuff

--- discrete_signals.py
+++ discrete_signals.py
@@ -1,12 +1,11 @@
 """Some functions for constructing/manipulating discrete signals."""
 
-import jax.numpy as np
+import jax.numpy as jnp
 from jax import vmap
 from jax.lax import conv_general_dilated
-from jax.ops import index_update, index
-from numpy.polynomial.legendre import Legendre
+from numpy.polynomial.chebyshev import Chebyshev
 
-def discretized_legendre(degree, N, ran=[-1,1]):
+def discretized_chebyshev(degree, N, ran=[-1,1]):
     """
     Discretized Legendre polynomial of specified degree over interval [0,T], with samples taken
     using midpoint rule
@@ -19,22 +18,22 @@ def discretized_legendre(degree, N, ran=[-1,1]):
 
     """
     dt = 1. / N
-    coeffs = np.zeros(degree + 1)
-    coeffs = index_update(coeffs, index[-1], 1.)
+    coeffs = jnp.zeros(degree + 1)
+    coeffs = coeffs.at[-1].set(1.)
 
-    return np.array(Legendre(coeffs, [0,1.], ran)(np.linspace(0, 1. - dt, N) + (dt / 2)))
+    return jnp.array(Chebyshev(coeffs, [0, 1.], ran)(jnp.linspace(0, 1. - dt, N) + (dt / 2)))
 
-def discretized_legendre_basis(max_degree, N, ran=[-1,1]):
+def discretized_chebyshev_basis(max_degree, N, ran=[-1,1]):
     """Return all discretized legendre polynomials up to a given degree."""
 
     # Note: I'd like to do the following line, but there is some jax issue
-    #return vmap(lambda deg: discretized_legendre(deg, T, N, ran))(np.array(list(range(max_degree+1))))
+    #return vmap(lambda deg: discretized_legendre(deg, T, N, ran))(jnp.array(list(range(max_degree+1))))
 
     vals = list(range(max_degree + 1))
-    disc_leg_map = map(lambda deg: discretized_legendre(deg, N, ran), vals)
+    disc_leg_map = map(lambda deg: discretized_chebyshev(deg, N, ran), vals)
 
     # convert to array and return
-    return np.array(list(disc_leg_map))
+    return jnp.array(list(disc_leg_map))
 
 def get_diffeo(image=[-1, 1]):
     """
@@ -43,7 +42,7 @@ def get_diffeo(image=[-1, 1]):
 
     width = image[1] - image[0]
 
-    return lambda x: np.arctan(x)/(np.pi/2) * (width / 2) + image[0] + (width / 2)
+    return lambda x: jnp.arctan(x)/(jnp.pi/2) * (width / 2) + image[0] + (width / 2)
 
 
 def fine_1d_conv(in_x, kernel, kern_sample_rate=1):
@@ -65,22 +64,22 @@ def fine_1d_conv(in_x, kernel, kern_sample_rate=1):
 
     # expand the input
     # this may actually be doable with conv_general_dilated
-    expanded = vmap(lambda x: x * np.ones(kern_sample_rate))(in_x).flatten()
+    expanded = vmap(lambda x: x * jnp.ones(kern_sample_rate))(in_x).flatten()
     kernel_len = len(kernel)
 
-    return conv_general_dilated(np.array([[expanded]]),
-                                np.array([[kernel]]),
+    return conv_general_dilated(jnp.array([[expanded]]),
+                                jnp.array([[kernel]]),
                                 (1,),
                                 padding=[(kernel_len,kernel_len)] )[0,0]
 
 # gaussian function
 def gaussian(x, amp, mean, sig):
     """compute value of a gaussian with certain parameters"""
-    return amp * np.exp(-np.power(x - mean, 2.) / (2 * np.power(sig, 2.)))
+    return amp * jnp.exp(-jnp.power(x - mean, 2.) / (2 * jnp.power(sig, 2.)))
 
 def gauss_conv_kernel(amp, sigma, samples, width):
     """constructs a discrete gaussian"""
-    return vmap(lambda t: gaussian(t, amp, 0, sigma))(np.linspace(-width, width, samples))
+    return vmap(lambda t: gaussian(t, amp, 0, sigma))(jnp.linspace(-width, width, samples))
 
 def get_gaussian_filter(samples, zero_tol=10**-4, kern_sample_rate=1):
     """Returns a function that convolves a 1d array with a discrete gaussian.
@@ -99,10 +98,10 @@ def get_gaussian_filter(samples, zero_tol=10**-4, kern_sample_rate=1):
     """
 
     # get x value for which gaussian of amp=1, mean=0, and std=sigma hits zero_tol
-    width = np.sqrt(- (2 * np.power(1., 2.)) * np.log(zero_tol))
+    width = jnp.sqrt(- (2 * jnp.power(1., 2.)) * jnp.log(zero_tol))
 
-    conv_kernel = gauss_conv_kernel(1., 1., samples, width)
-    conv_kernel = conv_kernel / np.sum(conv_kernel)
+    conv_kernel = gauss_conv_kernel(1., 3., samples, width)
+    conv_kernel = conv_kernel / jnp.sum(conv_kernel)
 
     return lambda in_x: fine_1d_conv(in_x, conv_kernel, kern_sample_rate)
 
@@ -114,7 +113,7 @@ def get_param_to_signal(leg_order, N):
     # derived parameters
     filter_samples_per_awg = int(4/filter_std) # number of samples of gaussian per sample of piecewise constant signal
     dt_filter = dt_awg / filter_samples_per_awg
-    N_filter = int(np.ceil( (3 * filter_std) / dt_filter )) # number of samples for the filter
+    N_filter = int(jnp.ceil( (3 * filter_std) / dt_filter )) # number of samples for the filter
     filter_time_width = N_filter * dt_filter
     gaussian = gauss_conv_kernel(1., filter_std, 2*N_filter, filter_time_width)
     gauss_conv = gaussian / gaussian.sum()
@@ -123,14 +122,15 @@ def get_param_to_signal(leg_order, N):
     apply_filter = lambda in_sig: fine_1d_conv(in_sig, gauss_conv, kern_sample_rate=filter_samples_per_awg)
 
     # create basis
-    signal_basis = discretized_legendre_basis(leg_order, N)
+    signal_basis = discretized_chebyshev_basis(leg_order, N)
 
     # create diffeomorphism from R into [-1, 1]
     diffeo = get_diffeo([-1, 1])
 
     def param_to_signal(params):
+
         # take linear combo of discretized legendre polynomials
-        leg_linear_combo = np.tensordot(params, signal_basis, axes=1)
+        leg_linear_combo = jnp.tensordot(params, signal_basis, axes=(1, 0))
 
         # map all samples into [-1,1]
         normalized = vmap(lambda x: diffeo(x), in_axes=1, out_axes=1)(leg_linear_combo)
