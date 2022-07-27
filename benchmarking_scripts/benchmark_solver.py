59cbcef98f4c20155c4a2b758e2d5de961737070
Ben Rosand <benjamin.rosand@ibm.com>
2022-07-27 14:01:27 -0400

    adding clean plotters

--- benchmarking_scripts/benchmark_solver.py
+++ benchmarking_scripts/benchmark_solver.py
@@ -1,4 +1,3 @@
-# %% [markdown]
 # # Perturbative solver demo
 #
 # This demo walks through the construction and usage of `PerturbativeSolver` objects for simulating a 2 transmon gate, comparing to traditional solvers using both dense and sparse arrays.
@@ -49,7 +48,7 @@ import jax
 jax.config.update("jax_enable_x64", True)
 
 
-def main_runner(
+def main(
     cpu_count,
     output_file,
     vmap_flag,
@@ -58,10 +57,6 @@ def main_runner(
     num_inputs=10,
     test_run=False,
 ):
-    # cpu_count=36
-    # output_file='notebook'
-    # vmap_flag=True
-    # arg_solv='dyson'
     test_path = "/u/brosand/projects"
     if os.path.exists(test_path):
         results_db_path = (
@@ -74,7 +69,6 @@ def main_runner(
 
     connection = create_connection(results_db_path)
     cursor = connection.cursor()
-    # cursor.execute(SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}')
     if (
         cursor.execute(
             "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='benchmarks'"
@@ -113,7 +107,6 @@ def main_runner(
         else:
             vmap = 0
 
-        # columns = ["solver", "jit_time", "ave_run_time", "ave_distance", "jit_grad", "ave_grad_run_time", "construction_time", "step_count", "tol", "cpus", "gpus", "cheb_order", "exp_order", "vmap"]
         columns = [
             solver,
             jit_time,
@@ -139,7 +132,6 @@ def main_runner(
 
         string_columns = ",".join(columns)
         cursor.execute(f"INSERT INTO benchmarks VALUES ({string_columns})")
-        # f"INSERT INTO benchmarks(solver {solver}, jit_time {jit_time}, ave_run_time {ave_run_time}, ave_distance {ave_distance}, jit_grad_time {jit_grad_time}, ave_grad_run_time {ave_grad_run_time}, construction_time {construction_time}, step_count {step_count}, tol {tol}, cpus {cpus}, gpus {gpus}, cheb_order {cheb_order}, exp_order {exp_order}, vmap {vmap})"
         connection.commit()
         logging.warning("committed to sql table")
 
@@ -198,7 +190,6 @@ def main_runner(
             funclist=[unipolar, lambda s: -unipolar(s - T / 2)],
         )
 
-    # %% [markdown]
     # Plot an example.
 
     # %%
@@ -211,10 +202,8 @@ def main_runner(
 
     sig = Signal(test)
 
-    # %%
     sig.draw(0, T, 1000, function="envelope")
 
-    # %% [markdown]
     # # 2. Construct model operators
     #
     # We construct a two transmon model:
@@ -259,7 +248,6 @@ def main_runner(
     Hdc = 2 * np.pi * (a0 + adag0)
     Hdt = 2 * np.pi * (a1 + adag1)
 
-    # %% [markdown]
     # ## 2.1 Get the dressed computational states qubit frequencies
 
     # %%
@@ -282,7 +270,6 @@ def main_runner(
         ind = get_dressed_state_index(inda, indb, dimension, evecs)
         return evals[ind], evecs[ind]
 
-    # %% [markdown]
     # Diagonalize and get dressed energies/states for computational states.
 
     # %%
@@ -302,7 +289,6 @@ def main_runner(
     Hdc_B = Badj @ Hdc @ B
     Hdt_B = Badj @ Hdt @ B
 
-    # %% [markdown]
     # Define fidelity with respect to the $Z \otimes X$ operator for the computational states.
 
     # %%
@@ -332,7 +318,6 @@ def main_runner(
     def fidelity(U):
         return jnp.abs(jnp.sum(target_conj * U)) ** 2 / (4 ** 2)
 
-    # %% [markdown]
     # # 3. Construct dense version of simulation
     #
     # Here we construct a function for simulating the system in the rotating frame of the drift, using a standard ODE solver, and dense arrays.
@@ -384,7 +369,6 @@ def main_runner(
     def ode_obj(params, tol):
         return fidelity(ode_sim(params, tol))
 
-    # %% [markdown]
     # ## Setup a collection of inputs values and create benchmark final unitaries
 
     # %%
@@ -411,7 +395,6 @@ def main_runner(
 
     benchmark_yfs = [benchmark_sim(x) for x in input_params]
 
-    # %% [markdown]
     # ## Create error metrics and function for running sims
 
     # %%
@@ -535,7 +518,6 @@ def main_runner(
 
     # %%
 
-    # %% [markdown]
     # # Dense simulation
     #
     # Run the sims for dense simulation at various tolerances.
@@ -552,7 +534,7 @@ def main_runner(
         gpu_count = 1
     else:
         gpu_count = 0
-    if arg_solv in ["dense", "all"]:
+    if arg_solv in ["ODE Solver", "all"]:
         dense_results = []
         for tol in tols:
             metrics = compute_solver_metrics(lambda params: ode_sim(params, tol))
@@ -560,7 +542,7 @@ def main_runner(
 
             write_result(
                 cursor=cursor,
-                solver="dense",
+                solver="ODE Solver",
                 jit_time=metrics["jit_time"],
                 ave_run_time=metrics["ave_run_time"],
                 ave_distance=metrics["ave_distance"],
@@ -584,7 +566,6 @@ def main_runner(
         # dense_results_df.to_csv(
         #     f"/u/brosand/danDynamics/multivariable_dyson_magnus/results/dense_results_{output_file}.csv"
         # )
-    # %% [markdown]
     # # Sparse version of simulation
     #
     # For sparse simulation we need to make sure we are in a basis in which the operators are actually sparse.
@@ -672,23 +653,21 @@ def main_runner(
         sparse_results_df.to_csv(
             f"/u/brosand/danDynamics/multivariable_dyson_magnus/results/sparse_results_{output_file}.csv"
         )
-    # %% [markdown]
     # # Dyson solver
 
-    # %%
     # system information
     operators = [-1j * Hdc_B, -1j * Hdt_B]
     carrier_freqs = [v_t, v_t]
     frame_operator = -1j * np.diag(H0_B)
 
     def perturbative_solver_metrics(
-        n_steps, expansion_order, chebyshev_order, expansion_method="dyson"
+        n_steps, expansion_order, chebyshev_order, expansion_method="Dyson"
     ):
         dt = T / n_steps
 
         # construct solver
         start = time()
-        if expansion_method == "dyson":
+        if expansion_method == "Dyson":
             perturb_solver = DysonSolver(
                 operators=operators,
                 rotating_frame=frame_operator,
@@ -749,17 +728,15 @@ def main_runner(
         results["construction_time"] = construction_time
         return results
 
-    # %% [markdown]
     # For reference, this is a value that gives a very high quality approximation.
 
-    # %%
     import warnings
 
     step_counts = [10000, 20000, 30000, 40000, 50000]
     exp_orders = [2, 3, 4, 5]
     cheb_orders = [0, 1, 2]
 
-    if arg_solv in ["dyson", "all"]:
+    if arg_solv in ["Dyson", "all"]:
         perturbative_results = []
         logging.warning("HIII")
         for step_count in step_counts:
@@ -772,7 +749,7 @@ def main_runner(
                         n_steps=step_count,
                         expansion_order=exp_order,
                         chebyshev_order=cheb_order,
-                        expansion_method="dyson",
+                        expansion_method="Dyson",
                     )
                     test["step_count"] = step_count
                     test["exp_order"] = exp_order
@@ -780,7 +757,7 @@ def main_runner(
                     metrics = test
                     write_result(
                         cursor=cursor,
-                        solver="dyson",
+                        solver="Dyson",
                         jit_time=metrics["jit_time"],
                         ave_run_time=metrics["ave_run_time"],
                         ave_distance=metrics["ave_distance"],
@@ -798,11 +775,7 @@ def main_runner(
                     )
                     perturbative_results.append(test)
 
-                    # pert_df = pd.DataFrame(perturbative_results)
-                    # # pert_df.to_csv('/u/brosand/danDynamics/multivariable_dyson_magnus/results/perturbative_results_cpu_dyson_{}.csv'.format(cpu_count))
-                    # pert_df.to_csv(
-                    #     f"/u/brosand/danDynamics/multivariable_dyson_magnus/results/dyson_results_{output_file}.csv"
-                    # )
+
 
     if arg_solv in ["magnus", "all"]:
         perturbative_results = []
@@ -843,19 +816,6 @@ def main_runner(
                         cheb_order=metrics["cheb_order"],
                     )
 
-                    # perturbative_results.append(test)
-
-                    # pert_df = pd.DataFrame(perturbative_results)
-                    # # pert_df.to_csv('/u/brosand/danDynamics/multivariable_dyson_magnus/results/perturbative_results_cpu_magnus_{}.csv'.format(cpu_count))
-                    # pert_df.to_csv(
-                    #     f"/u/brosand/danDynamics/multivariable_dyson_magnus/results/magnus_results_{output_file}.csv"
-                    # )
-
-
-# %% [markdown]
-# We should generate data for perturbative solvers, for both Dyson and Magnus, treating `n_steps` analogously to `tol` for the usual solvers. I.e. for different expansion orders and chebyshev orders, generate the metrics for a range of `n_steps`. We'll need to play around to see what actual values of `n_steps` to explore (e.g. cranking it really high to get as high a tolerance as possible).
-#
-# For CPU, can we pin this to a single core?
 
 # %%
 if __name__ == "__main__":
@@ -865,18 +825,29 @@ if __name__ == "__main__":
     parser.add_argument("--output_name")
     parser.add_argument("--cpus", default=8, type=int)
     parser.add_argument("--n_inputs", default=8, type=int)
-    parser.add_argument("--test", dest="test", action="store_true")
-    parser.add_argument("--vmap", dest="vmap", action="store_true")
-    parser.add_argument("--norft", dest="rft", action="store_false")
-    parser.set_defaults(rft=True)
-    parser.set_defaults(vmap=False)
-    parser.set_defaults(test=False)
+    parser.add_argument("--test", dest="test", action="store_true", default=False)
+    parser.add_argument("--vmap", dest="vmap", action="store_true", default=False)
+    parser.add_argument("--full_run", dest="full_run", action="store_true", default=False)
 
     args = parser.parse_args()
 
     logging.warning(f"arg_solv is {args.solver}")
+    if args.full_run:
+        solvers = ["ODE Solver", "Dyson", "magnus"]
+        n_inputs = [1, 50, 100]
+        for solver_choice in solvers:
+            for input_count in n_inputs:
+                main(
+                    cpu_count=args.cpus,
+                    output_file=args.output_name,
+                    arg_solv=solver_choice,
+                    vmap_flag=args.vmap,
+                    num_inputs=input_count,
+                    sql=args.sql,
+                    test_run=args.test
+                )
 
-    main_runner(
+    main(
         cpu_count=args.cpus,
         output_file=args.output_name,
         arg_solv=args.solver,
@@ -884,6 +855,4 @@ if __name__ == "__main__":
         num_inputs=args.n_inputs,
         sql=args.sql,
         test_run=args.test
-        # rft=args.rft,
-        # args_dict=vars(args),
     )
